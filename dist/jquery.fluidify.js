b2eedd5fc05ae034a4c6a1ac851fa991b2061363
Danijel Dedic <danijel.dedic@styleflasher.at>
2016-02-15 17:00:14 +0100

    wip

--- dist/jquery.fluidify.js
+++ dist/jquery.fluidify.js
@@ -1,70 +1,99 @@
 /*
- *  jquery-boilerplate - v3.5.0
- *  A jump-start for jQuery plugins development.
- *  http://jqueryboilerplate.com
+ *  jquery.fluidify - v0.5.0
+ *  jQuery plugin to make bootstrap containers more fluid.
+ *  http://github.com/ddedic/jquery.fluidify
  *
- *  Made by Zeno Rocha
+ *  Made by Danijel Dedic
  *  Under MIT License
  */
-// the semi-colon before function invocation is a safety net against concatenated
-// scripts and/or other plugins which may not be closed properly.
+
 ;(function ( $, window, document, undefined ) {
 
 	"use strict";
 
-		// undefined is used here as the undefined global variable in ECMAScript 3 is
-		// mutable (ie. it can be changed by someone else). undefined isn't really being
-		// passed in so we can ensure the value of it is truly undefined. In ES5, undefined
-		// can no longer be modified.
-
-		// window and document are passed through as local variable rather than global
-		// as this (slightly) quickens the resolution process and can be more efficiently
-		// minified (especially when both are regularly referenced in your plugin).
-
-		// Create the defaults once
-		var pluginName = "defaultPluginName",
-				defaults = {
-				propertyName: "value"
+		var pluginName = "jFluidify",
+			defaults = {
+                position: "relative",
+                width: "100",
+                gutter: 15,
+                breakpints: {
+	                lg: 1200,
+	                md: 992,
+	                sm: 768,
+	                xs: 480
+                }
 		};
 
-		// The actual plugin constructor
 		function Plugin ( element, options ) {
-				this.element = element;
-				// jQuery has an extend method which merges the contents of two or
-				// more objects, storing the result in the first object. The first object
-				// is generally empty as we don't want to alter the default options for
-				// future instances of the plugin
-				this.settings = $.extend( {}, defaults, options );
-				this._defaults = defaults;
-				this._name = pluginName;
-				this.init();
+			this.element = element;
+
+			this.settings = $.extend( {}, defaults, options );
+			this._defaults = defaults;
+			this._name = pluginName;
+			this.init();
 		}
 
 		// Avoid Plugin.prototype conflicts
 		$.extend(Plugin.prototype, {
-				init: function () {
-						// Place initialization logic here
-						// You already have access to the DOM element and
-						// the options via the instance, e.g. this.element
-						// and this.settings
-						// you can add more functions like the one below and
-						// call them like the example bellow
-						this.yourOtherFunction("jQuery Boilerplate");
-				},
-				yourOtherFunction: function (text) {
-						// some logic
-						$(this.element).text(text);
+			init: function () {
+				var me = this;
+				$(this.element).data('jFluidifyInitialWidth', $(this.element).width());
+				me.setupElement();
+
+			    $(window).resize(function(){
+			        console.log("resize event fired");
+			        me.setupElement();
+			    });
+
+
+
+			    var root_container = null;
+			    var $parent_containers = $(this.element).parents('.container');
+
+			    console.log($parent_containers);
+
+			    if ($parent_containers.size() > 1) {
+
+			    	console.log($parent_containers);
+
+			    	root_container = $parent_containers[$parent_containers.size() - 1];
+			    } else {
+			    	root_container = $parent_containers[0];
+			    }
+
+			    console.log(root_container);
+
+
+			},
+			setupElement: function () {
+				var window_width = $(window).width();
+				var element_width = $(this.element).data('jFluidifyInitialWidth');
+				var diff = (window_width - element_width) / 2;
+
+				console.log(element_width);
+
+				if (this.settings.width < 100) {
+					// @todo
 				}
+
+				$(this.element).css({
+					'position': this.settings.position,
+					'overflow': 'hidden',
+					'margin-left': '-' + diff + 'px',
+					'width': element_width + (diff * 2)  + 'px'
+				});
+
+			}
 		});
 
-		// A really lightweight plugin wrapper around the constructor,
-		// preventing against multiple instantiations
+
 		$.fn[ pluginName ] = function ( options ) {
-				return this.each(function() {
-						if ( !$.data( this, "plugin_" + pluginName ) ) {
-								$.data( this, "plugin_" + pluginName, new Plugin( this, options ) );
-						}
-				});
+			return this.each(function() {
+				if ( !$.data( this, "plugin_" + pluginName ) ) {
+					$.data( this, "plugin_" + pluginName, new Plugin( this, options ) );
+				}
+			});
 		};
 
 })( jQuery, window, document );
+
